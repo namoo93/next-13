4a40468ff741b05250b96474ba3e5db1b88adaab
HyoSean <kimhyosun@gmail.com>
2023-04-19 20:07:23 +0900

    add

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\skqld\\Documents\\GitHub\\grotesq\\next-13\\app\\layout.tsx": [
+      "C:\\Users\\skqld\\Documents\\GitHub\\grotesq\\next-13\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\skqld\\Documents\\GitHub\\grotesq\\next-13\\app\\page": [
+      "C:\\Users\\skqld\\Documents\\GitHub\\grotesq\\next-13\\app\\globals.css"
+    ]
+  }
+}
