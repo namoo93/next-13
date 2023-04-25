1a6beed375ed0e5983e7cc2deca1670b7c677717
HyoSean <kimhyosun@gmail.com>
2023-04-26 08:44:07 +0900

    add

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -6,10 +6,18 @@ import LinkButton from './LinkButton';
 import { ItemType } from '../../types/ItemType';
 
 export const dynamic = 'force-dynamic';
+// export const revalidate = 60; //페이지 방문시 60초동안 캐싱
 
 export default async function List() {
   let db = (await connectDB).db('forum');
   let result = (await db.collection('post').find().toArray()) as ItemType[];
+
+  //get요청결과 캐싱
+  // await fetch('/url', { cache: 'force-cache' });// 기본값 생략가능 매번 get가져옴
+  // await fetch('/url', { cache: 'no-store' }); //실시간 데이터를위한 캐싱x
+  // await fetch('/url', { next: {revalidate: 60} }); //60초간 캐싱 데이터보관
+  // - Next.js에선 쌩자바스크립트의 fetch() 기본함수를 업그레이드해놔서 사용가능한 문법입니다.
+  // - server component 안에서만 캐싱기능 사용가능
 
   // console.log(result);
   return (
