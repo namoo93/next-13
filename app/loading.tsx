bfd9ede04eef6573f6bf80bc43b3bdf206064b4f
HyoSean <kimhyosun@gmail.com>
2023-05-28 22:36:27 +0900

    add

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,10 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
 export default function Loading() {
-  return <>loading...</>;
+  return (
+    <div>
+      <Skeleton />
+    </div>
+  );
 }
